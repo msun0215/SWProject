758d775972b5ee877a3ac167d889e4e775d106dd
SeungWon Kim <hesjs5@skuniv.ac.kr>
2023-12-04 10:42:20 +0900

    refactor: 죽은 코드 삭제

--- src/main/java/com/example/BoardDBRestAPIBySpring/controller/PostController.java
+++ src/main/java/com/example/BoardDBRestAPIBySpring/controller/PostController.java
@@ -1,18 +1,13 @@
 package com.example.BoardDBRestAPIBySpring.controller;
 
 import com.example.BoardDBRestAPIBySpring.domain.Board;
-import com.example.BoardDBRestAPIBySpring.request.PostCreateRequest;
 import com.example.BoardDBRestAPIBySpring.response.PostResponse;
 import com.example.BoardDBRestAPIBySpring.service.PostService;
-import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,21 +33,5 @@ public class PostController {
 		return postsByPaging.stream()
 			.map(PostResponse::new)
 			.toList();
-	}
-
-	@GetMapping("/counts")
-	public long getPostCounts() {
-		return postService.calculatePostCounts();
-	}
-
-	// 참조
-	// https://velog.io/@sunclock/Postman%EC%97%90%EC%84%9C-file%EA%B3%BC-json-body-%ED%95%9C%EB%B2%88%EC%97%90-POST-%EC%9A%94%EC%B2%AD%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95
-	@PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE,
-		MediaType.APPLICATION_OCTET_STREAM_VALUE})
-	public void create(@ModelAttribute @Valid PostCreateRequest request) {
-		log.info("postCreateRequest = {}", request);
-		log.info("postCreateRequest.files size = {}", request.getFiles().size());
-
-		Board board = postService.save(request);
 	}
 }
